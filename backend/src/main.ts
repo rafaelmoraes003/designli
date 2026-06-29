820bc5411d5725320b3a3009244bd9cdd8cfa463
rafael moraes <rafaelsm003@gmail.com>
2026-06-29 13:57:27 -0300

    feat: enable CORS support in the main application bootstrap

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
